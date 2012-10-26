29496a25bf3e99e0a5f488f7fd3d988836397aa4
Asheesh Laroia <asheesh@asheesh.org>
2012-10-25 22:14:29 -0400

    Adding a setup.py
    This is a very simple setup.py, created by looking at
    http://guide.python-distribute.org/quickstart.html
    
    It uses "." as the package_dir to avoid needing a src/ directory.
    
    It sets the maintainer contact information to mad's, and and uses
    the Github URL as the project website, which seems to make sense
    to me.

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='python-gedcom',
+    version='0.1dev',
+    packages=['gedcom',],
+    license='GPLv2',
+    package_dir={'': '.'},
+    description=open('README').readlines()[0].strip(),
+    long_description=open('README').read(),
+    maintainer='Madeleine Ball',
+    maintainer_email='meprice@gmail.com',
+    url='https://github.com/madprime/python-gedcom',
+)
