56208d6436fd8b075bc58af8db306e51df3b77dc
Madeleine Ball <mpball@gmail.com>
2012-11-03 19:19:50 -0400

    Updating email address

--- setup.py
+++ setup.py
@@ -9,6 +9,6 @@ setup(
     description=open('README').readlines()[0].strip(),
     long_description=open('README').read(),
     maintainer='Madeleine Ball',
-    maintainer_email='meprice@gmail.com',
+    maintainer_email='mpball@gmail.com',
     url='https://github.com/madprime/python-gedcom',
 )
